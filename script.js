a12d2cf8ca0df8757c58bcf3c47eeecc46b2391f
Sk Ajhar Uddin <62983361+SKAJHARUDDIN@users.noreply.github.com>
2021-09-11 21:56:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//  Event Handler
+
+let myVar = document.querySelector("#myPictureId");
+function myPicture1(){
+    myVar.src = "img/k.jpeg";
+}
+
+function myPicture2(){
+    myVar.src = "img/letscode.jpg";
+}
